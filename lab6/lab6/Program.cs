996cdd1942f24e66f4d7a586b79e2f17bdd7091e
Andre Otte <andre.b.otte1@gmail.com>
2019-04-25 14:40:10 -0400

    it works

--- lab6/lab6/Program.cs
+++ lab6/lab6/Program.cs
@@ -10,6 +10,32 @@ namespace lab6
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the number of sides on the dice: ");
+            int sideNumber = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter the number of dice you want to roll: ");
+            int diceNumber = Convert.ToInt16(Console.ReadLine());
+
+            int maxRoll = Roll(sideNumber, diceNumber);
+
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int output = GetRandom(maxRoll, rand);
+                Console.WriteLine(output);
+            }
+        }
+
+        public static int GetRandom(int max, Random rand)
+        {
+            int number = rand.Next(1, max + 1);
+            return number;
+        }
+
+        public static int Roll(int sides, int dice)
+        { 
+            int maxRoll = sides * dice;
+            return maxRoll;
         }
     }
 }
